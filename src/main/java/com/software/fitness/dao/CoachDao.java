9de4283792e6f2bfe61728d804a99f62ea917106
ShiningZ <1357591485@qq.com>
2020-07-05 21:54:24 +0800

    教练管理框架代码 zcf 2020-7-5

---
+++ src/main/java/com/software/fitness/dao/CoachDao.java
@@ -0,0 +1,13 @@
+package com.software.fitness.dao;
+
+import com.software.fitness.domain.Coach;
+import org.apache.ibatis.annotations.Mapper;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+@Mapper
+public interface CoachDao {
+
+}
